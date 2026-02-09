ee77d039cf1cd533b435e0f05a1aec5ceeae23b0
Uwe_Knopf <bob@fake.com>
2026-02-09 21:24:45 +0100

    fix: make TCGDex conform to Disposable again

--- net-sdk/src/TCGDex.cs
+++ net-sdk/src/TCGDex.cs
@@ -10,7 +10,7 @@ public interface ITCGDex
     Task<Card> fetchCard(string CardID);
 }
 
-public class TCGDex: ITCGDex {
+public class TCGDex: ITCGDex, IDisposable {
     readonly RestClient _client;
 
     public TCGDex(string language)
