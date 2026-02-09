a86cf27fda8f10d108618378fb46c8c326573846
Uwe_Knopf <bob@fake.com>
2026-02-09 18:10:48 +0100

    feat: add card model

--- net-sdk/src/models/Card.cs
+++ net-sdk/src/models/Card.cs
@@ -1,3 +1,35 @@
+using net_sdk.src.models.subs;
+
 namespace net_sdk.src.models;
 
-public record class Card(string id, string name);
+public record class Card(
+    string id,
+    string localId,
+    string name,
+    string? image,
+    string? illustrator,
+    string rarity,
+    string category,
+    CardVariants? varients,
+    SetResume set,
+    Pricing? pricing,
+    List<int>? dexId,
+    int? hp,
+    List<string>? types,
+    string? evolveFrom,
+    string? description,
+    string? level,
+    string? stage,
+    string? suffix,
+    CardItem? item,
+    List<CardAbility> abilities,
+    List<CardAttack> attacks,
+    List<CardWeakRes> weaknesses,
+    List<CardWeakRes> resistances,
+    int? retreat,
+    string? effect,
+    string? trainerType,
+    string? energyType,
+    string? regulationMark,
+    Legal legal
+    );
