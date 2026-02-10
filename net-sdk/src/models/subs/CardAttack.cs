bd8be4c8df8e8773e309b5b7386c3af098091fce
Uwe_Knopf <bob@fake.com>
2026-02-10 10:27:00 +0100

    Fix: wrong type for damage

--- net-sdk/src/models/subs/CardAttack.cs
+++ net-sdk/src/models/subs/CardAttack.cs
@@ -2,4 +2,4 @@ using System;
 
 namespace net_sdk.src.models.subs;
 
-public record class CardAttack(string name, List<String>? cost = null, string? effect = null, string? damage = null);
+public record class CardAttack(string name, List<String>? cost = null, string? effect = null, int? damage = null);
