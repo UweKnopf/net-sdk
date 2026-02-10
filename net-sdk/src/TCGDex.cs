UweKnopf/net-sdk

commit b44e6204663b5cc57afe0d9f628b16e0b5193f43

fix: language parameter now gets used

net-sdk/src/TCGDex.cs
@@ -15,14 +15,14 @@ public class TCGDex: ITCGDex {
 
     public TCGDex(string language)
     {
-        var options = new RestClientOptions("https://api.tcgdex.net/v2");
+        var options = new RestClientOptions("https://api.tcgdex.net/v2/{language}");
         _client = new RestClient(options);
     }
 
     public async Task<Card> fetchCard(string CardID)
     {
         var response = await _client.GetAsync<Card>(
-            "/en/cards/{CardID}",
+            "/cards/{CardID}",
             new { CardID }
         );
         return response!;

net-sdkTest/Test1.cs
@@ -10,7 +10,7 @@ public sealed class Test1
     [TestMethod]
     public async Task TestMethod1()
     {
-        TCGDex sdk = new TCGDex(language: "");
+        TCGDex sdk = new TCGDex(language: "en");
         var a = await sdk.fetchCard("swsh3-136");
         //a.ToString;
         Console.Write(a.name);

commit 9f05e1f29fcd80fe808d60e27b0b2711098c0d4e

Fix: regression caused by b44e6204663b5cc57afe0d9f628b16e0b5193f43

net-sdk/src/TCGDex.cs
@@ -16,15 +16,15 @@ public class TCGDex: ITCGDex, IDisposable {
 
     public TCGDex(string language)
     {
-        var options = new RestClientOptions("https://api.tcgdex.net/v2/{language}");
+        var options = new RestClientOptions("https://api.tcgdex.net/v2/" + language);
         _client = new RestClient(options);
         _client.AddDefaultHeader("user-agent", "@UweKnopf/net-sdk");
     }
 
-    public Task<Card> fetchCard(string CardID)
+    public async Task<Card> fetchCard(string CardID)
     {
-        var response = fetch<Card>(CardID);
-        return response;
+        var response = await fetch<Card>(CardID);
+        return response!;
     }
 
     public void Dispose() {
@@ -35,7 +35,7 @@ public class TCGDex: ITCGDex, IDisposable {
     private async Task<T> fetch<T>(string fetchParam)
     {
         var response = await _client.GetAsync<T>(
-            "/cards/{CardID}",
+            "/cards/{fetchParam}",
             new { fetchParam }
         );
 