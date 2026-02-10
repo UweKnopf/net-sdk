efd858e8992f08c3cb72131747b557def50a5e33
Uwe_Knopf <bob@fake.com>
2026-02-10 16:54:29 +0100

    feat: add tcgplayer pricing sum models

--- net-sdk/src/models/subs/PricingTcgPlayer.cs
+++ net-sdk/src/models/subs/PricingTcgPlayer.cs
@@ -1,6 +1,13 @@
 namespace net_sdk.src.models.subs;
 
-public record class PricingTcgPlayer
-{
-
-}
+public record class PricingTcgPlayer(
+    string updated,
+    string unit,
+    PricingTcgPlayerVariant? normal,
+    PricingTcgPlayerVariant? holoFoil,
+    PricingTcgPlayerVariant? reverseHolo,
+    PricingTcgPlayerVariant? firstEdition,
+    PricingTcgPlayerVariant? firstEditionHolofoil,
+    PricingTcgPlayerVariant? unlimited,
+    PricingTcgPlayerVariant? unlimitedHolofoil
+);
--- net-sdk/src/models/subs/PricingTcgPlayerVariant.cs
+++ net-sdk/src/models/subs/PricingTcgPlayerVariant.cs
@@ -1,6 +1,9 @@
 namespace net_sdk.src.models.subs;
 
-public record class PricingTcgPlayerVariant
-{
-
-}
+public record class PricingTcgPlayerVariant(
+    float? lowPrice,
+    float? midPrice,
+    float? highPrice,
+    float? marketPrice,
+    float? directLowPrice
+);
