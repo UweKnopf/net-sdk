329dbe494d416307c4c8b3c4393b0f1faf0e75d4
Uwe_Knopf <bob@fake.com>
2026-03-05 19:55:36 +0100

    Doc: Set Resume

--- net-sdk/src/models/SetResume.cs
+++ net-sdk/src/models/SetResume.cs
@@ -11,12 +11,21 @@ public record class SetResume(
     SetCardCount CardCount
 ) : Model()
 {
+    /// <summary>
+    /// Async returns the logo url of the set, based on the <see cref="Logo"/> property and the given <paramref name="extension"/>.
+    /// </summary>
+    /// <param name="extension"></param>
+    /// <returns>Returns the logo url of the set as a string.</returns>
     public string? GetLogoUrl(Extension extension)
     {
         if (Logo == null) return null;
         return $"{Logo}.{extension}";
     }
-
+    /// <summary>
+    /// Async returns the logo of the set, based on the <see cref="Logo"/> property and the given <paramref name="extension"/>.
+    /// </summary>
+    /// <param name="extension"></param>
+    /// <returns>Returns the logo of the set as a byte array.</returns>
     public byte[]? GetLogo(Extension extension)
     {
         var logoUrl = GetLogoUrl(extension);
@@ -24,13 +33,21 @@ public record class SetResume(
         return TCGDex.GetImage(logoUrl);
         
     }
-
+    /// <summary>
+    /// Async returns the symbol url of the set, based on the <see cref="Symbol"/> property and the given <paramref name="extension"/>.
+    /// </summary>
+    /// <param name="extension"></param>
+    /// <returns>Returns the symbol url of the set as a string.</returns>
     public string? GetSymbolUrl(Extension extension)
     {
         if (Symbol == null) return null;
         return $"{Symbol}.{extension}";
     }
-
+    /// <summary>
+    /// Async returns the symbol of the set, based on the <see cref="Symbol"/> property and the given <paramref name="extension"/>.
+    /// </summary>
+    /// <param name="extension"></param>
+    /// <returns>Returns the symbol of the set as a byte array.</returns>
     public byte[]? GetSymbol(Extension extension)
     {
         var symbolUrl = GetSymbolUrl(extension);
@@ -38,7 +55,10 @@ public record class SetResume(
         return TCGDex.GetImage(symbolUrl);
         
     }
-
+    /// <summary>
+    /// Async returns a <see cref="Set"/> of the SetResume.
+    /// </summary>
+    /// <returns>Returns the full set as a <see cref="Set"/>.</returns>
     public async Task<Set?> GetFullSet()
     {
         return await TCGDex.FetchSet(Id);
