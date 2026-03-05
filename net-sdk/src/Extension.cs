957b117ad53f46d7e011480ab0857305e16833bc
Uwe_Knopf <bob@fake.com>
2026-03-05 20:37:29 +0100

    Doc: Misc enums

--- net-sdk/src/Extension.cs
+++ net-sdk/src/Extension.cs
@@ -1,9 +1,13 @@
 namespace net_sdk.src;
 
+/// <summary>
+/// The extension of the image to be fetched. Webp is highly recommended, especially if you want to display the images in a web application.
+/// PNG should be used if you need the transparent background.
+/// </summary>
 public enum Extension
 {
     //needs to be lowercase because custom enum string values are not possible
     png,
     jpg,
-    wepd,
+    webp,
 }
--- net-sdk/src/Quality.cs
+++ net-sdk/src/Quality.cs
@@ -1,5 +1,8 @@
 namespace net_sdk.src;
 
+/// <summary>
+/// The quality of the image to be fetched. High quality images are 600x825 pixels, low quality images are 245x337 pixels.
+/// </summary>
 public enum Quality
 {
     //needs to be lowercase because custom enum string values are not possible
